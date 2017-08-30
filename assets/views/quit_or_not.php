b76cffab4cbb5cbd6652070cf11cc15b28ef1234
Khoo Yong Jun <khoo.jack@gmail.com>
2017-08-30 16:15:50 +0800

    update

---
+++ assets/views/quit_or_not.php
@@ -0,0 +1,8 @@
+<div class="fixed myModal">
+	<div class="fixed quitbackground"></div>
+	<div class="flex-center">
+		<img src="assets/imgs/quit_or_not.png">
+	</div>
+	<div class="fixed mybutton" id="yes"></div>
+	<div class="fixed mybutton" id="no"></div>
+</div>

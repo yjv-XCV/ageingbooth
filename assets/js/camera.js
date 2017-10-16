5cd4b2532e8cc7eac2c36a3145154e60365b9ebe
Khoo Yong Jun <khoo.jack@gmail.com>
2017-10-16 17:24:35 +0800

    rollback 1

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -1,40 +1,40 @@
 var init_cam = function() {
 
-		  // getUserMedia init
-		  navigator.getMedia = ( navigator.getUserMedia ||
-		                        navigator.webkitGetUserMedia ||
-		                        navigator.mozGetUserMedia ||
-		                        navigator.msGetUserMedia);
+      // getUserMedia init
+      navigator.getMedia = ( navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia);
 
-		  if(!navigator.getMedia){
-		    alert("Your browser doesn't have support for the navigator.getUserMedia interface.");
-		  }
+      if(!navigator.getMedia){
+        alert("Your browser doesn't have support for the navigator.getUserMedia interface.");
+      }
 
-		  // getUserMedia
-		  navigator.getMedia(
-		    {
-		      video: {
-		        width: { max: 1012 },
-		        height: { max: 740 }
-		      }
-		    },
-		    function(stream){
-		      var cam = $('#cam-stream');
-		      cam.attr('src', window.URL.createObjectURL(stream));
-		      cam[0].play();
-		    },
-		    // Error Callback
-		    function(err){
-		      alert("There was an error with accessing the camera stream: " + err.name, err);
-		    }
-		  );
-		};
+      // getUserMedia
+      navigator.getMedia(
+        {
+          video: {
+            width: { max: 1012 },
+            height: { max: 740 }
+          }
+        },
+        function(stream){
+          var cam = $('#cam-stream');
+          cam.attr('src', window.URL.createObjectURL(stream));
+          cam[0].play();
+        },
+        // Error Callback
+        function(err){
+          alert("There was an error with accessing the camera stream: " + err.name, err);
+        }
+      );
+    };
 
 var take_snapshot_from_cam_stream = function(filetype, quality){
 
   var cam_stream = $('#cam-stream');
   var snap_canvas = $('#snap-canvas'),
-  	  snap_canvas2 = $('#image'),
+      snap_canvas2 = $('#image'),
       context = snap_canvas[0].getContext('2d'),
       context2 = snap_canvas2[0].getContext('2d');
 
@@ -51,14 +51,14 @@ var take_snapshot_from_cam_stream = function(filetype, quality){
 
   // Make a copy of the current frame in the video on the canvas.
   context.translate(height/2, width/2);
-  // context.rotate(Math.PI/2);
-  context.scale(-1,1);
-  context.drawImage(cam_stream[0], 113, 0, 740/1012*740/2, 470, -height/2, -width/2, height, width);
+  context.rotate(Math.PI/2);
+  context.scale(-1,-1);
+  context.drawImage(cam_stream[0], -width/2, -height/2, width, height);
 
-  context2.translate(width/2, height/2);
-  // context2.rotate(Math.PI/2);
-  context2.scale(-1,1);
-  context2.drawImage(cam_stream[0], -width/2, -height/2, height, width);
+  context2.translate(height/2, width/2);
+  context2.rotate(Math.PI/2);
+  context2.scale(-1,-1);
+  context2.drawImage(cam_stream[0], -width/2, -height/2, width, height);
 
   // Turn the canvas image into a dataURL that can be used as a src for our photo.
   return snap_canvas[0].toDataURL('image/' + filetype, quality);
